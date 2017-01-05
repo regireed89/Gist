5d455e72a75df8ad88e7b438dfcb87cddc433653
regireed89 <regireed89@gmail.com>
2017-01-05 10:12:44 -0600

    1.5.17

--- Gist 1 1.5.17/Program.cs
+++ Gist 1 1.5.17/Program.cs
@@ -10,6 +10,41 @@ namespace Gist_1_1._5._17
     {
         static void Main(string[] args)
         {
+            List<T> number = new List<T>();
+
+            for (int i = 0; i <= 100; i++)
+            {
+                T num = new T();
+                if (i % 2 == 0)
+                {
+                    number.Add(num);
+                    Console.WriteLine(i);
+                    Console.ReadLine();                 
+                }
+            }
+
+            for (int i = 0; i <= 100; i++)
+            {
+                T num = new T();
+                if (i % 2 == 0)
+                {
+                    number.Add(num);
+                    Console.ReadLine();
+                    if (i % 10 != 0)
+                    {
+                        number.Remove(num);
+                        Console.WriteLine(i);
+                        Console.ReadLine();
+                    }
+                }
+            }
         }
     }
 }
+
+    class T
+    {
+        public T() { }
+    }
+  
+
